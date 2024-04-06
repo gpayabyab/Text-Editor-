7ef25ea924b7085254233952216d4cb404d37f16
Gabrielle 🌿 <gabrielle@gabrieleslaptop.mynetworksettings.com>
2024-04-06 11:34:17 -0400

    added logic to a method that adds some content to the database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,6 +15,26 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
+// add to database portion 
+export const postDb = async (content) => {
+  console.log('Post to the database');
+
+  // Create a connection to the database database and version we want to use.
+  const todosDb = await openDB('todos', 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = todosDb.transaction('todos', 'readwrite');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('todos');
+
+  // Use the .add() method on the store and pass in the content.
+  const request = store.add({ todo: content });
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('Data saved to the database', result);
+};
 
 export const putDb = async (content) => console.error('putDb not implemented');
 
